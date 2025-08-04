c1d874f2f19b28383b28c00aea0ba67c77af196e
kert <franciskertdn@gmail.com>
2025-08-04 23:58:25 +0800

    initial

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -5,12 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 
-
 class ShopController extends Controller
 {
     public function showProducts()
     {
-        $products = Product::all(); // Fetch all products
-        return view('admin.product', compact('products')); // Pass to Blade
+        $products = Product::all();
+        return view('admin.product', compact('products')); // ✅ correct here
     }
 }
